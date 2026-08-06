6a461d66fbbb89eba3e32a970ffdb461d0bcb8b0
student <student@example.com>
2026-08-06 11:48:16 +0900

    🔍 feat: 게시글 단건 조회 API 구현 (#2)

--- hivits_backend/crud.py
+++ hivits_backend/crud.py
@@ -13,22 +13,3 @@ def create_post(db: Session, post_in: schemas.PostCreate) -> models.Post:
 def get_post(db: Session, post_id: int):
     return db.query(models.Post).filter(models.Post.id == post_id).first()
 
-
-def get_posts(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Post).offset(skip).limit(limit).all()
-
-
-def update_post(db: Session, db_post: models.Post, post_in: schemas.PostUpdate):
-    if post_in.title is not None:
-        db_post.title = post_in.title
-    if post_in.content is not None:
-        db_post.content = post_in.content
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-    return db_post
-
-
-def delete_post(db: Session, db_post: models.Post):
-    db.delete(db_post)
-    db.commit()
